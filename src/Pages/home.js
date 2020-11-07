fd7122b55ef2ba22f0a53ca9f998d1b03c576c1a
Adefemi Oseni <adefemi.oseni@algorismng.com>
2020-11-07 06:30:11 +0100

    updated

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import userDetail from "../assets/userdetail.png";
 import favorite from "../assets/star.png";
 import smiley from "../assets/smiley.png";
@@ -6,74 +6,84 @@ import send from "../assets/send.png";
 import settings from "../assets/settings.png";
 import search from "../assets/search.png";
 import logout from "../assets/logout.png";
-import { ChatBubble, UserAvatar, UserMain } from "./homeComponents";
+import {
+  ChatBubble,
+  UserAvatar,
+  UserMain,
+  ProfileModal,
+} from "./homeComponents";
 
 const Home = (props) => {
+  const [showProfile, setShowProfile] = useState(false);
+
   return (
-    <div className="home-container">
-      <div className="side">
-        <div className="flex align-center justify-between top">
-          <UserAvatar noStatus isV2 />
-          <img src={settings} />
-        </div>
+    <>
+      <ProfileModal visible={showProfile} close={() => setShowProfile(false)} />
+      <div className="home-container">
+        <div className="side">
+          <div className="flex align-center justify-between top">
+            <UserAvatar noStatus isV2 />
+            <img src={settings} onClick={() => setShowProfile(true)} />
+          </div>
 
-        <div className="searchCon">
-          <img src={search} />
-          <input placeholder="Search users" />
-        </div>
+          <div className="searchCon">
+            <img src={search} />
+            <input placeholder="Search users" />
+          </div>
 
-        <div className="userList">
-          <UserMain />
-          <UserMain />
-          <UserMain />
-          <UserMain />
-          <UserMain />
-          <UserMain />
-          <UserMain />
-          <UserMain />
-          <UserMain />
-          <UserMain />
-          <UserMain />
-          <UserMain />
-          <UserMain />
-          <UserMain />
-        </div>
-        <div className="logout">
-          <img src={logout} />
-          <div>logout</div>
-        </div>
-      </div>
-      <div className="main">
-        <div className="flex align-center justify-between heading">
-          <UserAvatar />
-          <div className="flex align-center rightItems">
-            <img src={favorite} />
-            <img src={userDetail} />
+          <div className="userList">
+            <UserMain />
+            <UserMain />
+            <UserMain />
+            <UserMain />
+            <UserMain />
+            <UserMain />
+            <UserMain />
+            <UserMain />
+            <UserMain />
+            <UserMain />
+            <UserMain />
+            <UserMain />
+            <UserMain />
+            <UserMain />
+          </div>
+          <div className="logout">
+            <img src={logout} />
+            <div>logout</div>
           </div>
         </div>
-        <div className="chatArea">
-          <ChatBubble bubbleType="" />
-          <ChatBubble bubbleType="sender" />
-          <ChatBubble bubbleType="sender" />
-          <ChatBubble bubbleType="" />
-          <ChatBubble bubbleType="" />
-          <ChatBubble bubbleType="sender" />
-          <ChatBubble bubbleType="sender" />
-          <ChatBubble bubbleType="" />
-          <ChatBubble bubbleType="" />
-          <ChatBubble bubbleType="sender" />
-          <ChatBubble bubbleType="sender" />
-          <ChatBubble bubbleType="" />
-        </div>
-        <div className="messageZone">
-          <div className="flex align-center justify-between topPart">
-            <img src={smiley} />
-            <img src={send} />
+        <div className="main">
+          <div className="flex align-center justify-between heading">
+            <UserAvatar />
+            <div className="flex align-center rightItems">
+              <img src={favorite} />
+              <img src={userDetail} />
+            </div>
           </div>
-          <textarea placeholder="Type your message here..." rows="5" />
+          <div className="chatArea">
+            <ChatBubble bubbleType="" />
+            <ChatBubble bubbleType="sender" />
+            <ChatBubble bubbleType="sender" />
+            <ChatBubble bubbleType="" />
+            <ChatBubble bubbleType="" />
+            <ChatBubble bubbleType="sender" />
+            <ChatBubble bubbleType="sender" />
+            <ChatBubble bubbleType="" />
+            <ChatBubble bubbleType="" />
+            <ChatBubble bubbleType="sender" />
+            <ChatBubble bubbleType="sender" />
+            <ChatBubble bubbleType="" />
+          </div>
+          <div className="messageZone">
+            <div className="flex align-center justify-between topPart">
+              <img src={smiley} />
+              <img src={send} />
+            </div>
+            <textarea placeholder="Type your message here..." />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/Pages/homeComponents.js
+++ src/Pages/homeComponents.js
@@ -1,4 +1,6 @@
 import React from "react";
+import close from "../assets/close.png";
+import edit from "../assets/edit.png";
 
 export const UserMain = (props) => {
   return (
@@ -37,6 +39,54 @@ export const ChatBubble = (props) => {
           access how content would be shown
         </p>
         <div className="time">08:22</div>
+      </div>
+    </div>
+  );
+};
+
+export const ProfileModal = (props) => {
+  return (
+    <div className={`modalContain ${props.visible ? "open" : ""}`}>
+      <div className="content-inner">
+        <div className="header">
+          <div className="title">Update Profile</div>
+          <img src={close} onClick={props.close} />
+        </div>
+        <div className="content">
+          <div className="inner">
+            <div className="leftHook">
+              <div
+                className="imageCon"
+                style={{
+                  backgroundImage: `url("https://cdn.pixabay.com/photo/2017/05/25/21/26/bird-feeder-2344414__340.jpg")`,
+                }}
+              />
+              <div className="point">
+                Change Picture
+                <img src={edit} />
+              </div>
+            </div>
+            <div className="dataInput">
+              <label>
+                <span>First name</span>
+                <input />
+              </label>
+              <label>
+                <span>Last name</span>
+                <input />
+              </label>
+              <label>
+                <span>Caption</span>
+                <input />
+              </label>
+              <label>
+                <span>About</span>
+                <textarea />
+              </label>
+            </div>
+          </div>
+          <button>Update</button>
+        </div>
       </div>
     </div>
   );
